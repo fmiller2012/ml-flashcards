ac4bd210f599bc953632607f196098bf8cd7756a
faraji miller <farajimiller@farajis-MacBook-Pro.local>
2025-02-06 00:36:32 -0500

    fixed path

--- ml-flashcards/src/components/Flashcard.jsx
+++ ml-flashcards/src/components/Flashcard.jsx
@@ -4,10 +4,12 @@ import FlashcardText from "./FlashcardText.jsx";
 import { useState } from "react";
 
 const Flashcard = ({question, answer, isFlipped, setIsFlipped, color, image}) => {
+  console.log("Image URL:", image);
+  console.log("Resolved Path:", `/images/${image}`);
   return (
     <div className={`flashcard-container ${isFlipped ? "flipped" : ""}`} onClick={() => setIsFlipped(!isFlipped)}>
       <div className="flashcard-inner">
-        <div className="flashcard-background" style={{ backgroundImage: `url(${image})` }}></div>
+        <div className="flashcard-background" style={{ backgroundImage: `url(/${image})` }}></div>
         <div className={`flashcard-overlay ${color}`}>
         <FlashcardText text={question} />
         </div>
